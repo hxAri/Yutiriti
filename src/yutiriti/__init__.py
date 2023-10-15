2256ae33318878ba58c3b08d1fc855d047c928f2
hxAri <hxari@proton.me>
2023-10-15 23:47:58 +0700

    Update

---
+++ src/yutiriti/__init__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+#
+# @author Ari Setiawan
+# @create 15.10-2023 23:39
+# @github https://github.com/hxAri/Yutiriti
+#
+# Kanashī Copyright (c) 2022 - Ari Setiawan <hxari@proton.me>
+# Kanashī Licence under GNU General Public Licence v3
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+#
+# Kanashī is not affiliated with or endorsed, endorsed at all by
+# Instagram or any other party, if you use the main account to use this
+# tool we as Coders and Developers are not responsible for anything that
+# happens to that account, use it at your own risk, and this is Strictly
+# not for SPAM.
+#
